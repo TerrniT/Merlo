2c31b9d1f3dc7ce5c080df8d98293fcd53f863d1
TerrniT <terrnit.megaman@gmail.com>
2023-02-05 23:57:37 +0500

    feat(routes): add page routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Route , Routes} from "react-router-dom"
+import {Home, Profile, CreateCampaign, CampignDetails} from "./pages"
 
 const App = () => {
-  return <div className="bg-green-500 text-black">app</div>;
+  return (
+    <div className="relative sm:-8 p-4 bg-zinc-900 min-h-screen flex">
+      <div className="sm:flex hidden mr-10 relative ">
+        sidebar
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        navbar
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Profile/>}/>
+        <Route path="/" element={<CreateCampaign/>}/>
+        <Route path="/" element={<CampignDetails/>}/>
+      </Routes>
+    </div>
+  )
 };
 
 export default App;
