24f38af257e973a15cff1954c898221055c5ff15
TerrniT <terrnit.megaman@gmail.com>
2023-03-01 01:00:43 +0500

    style(lint): fix prettier eslint conflicts

--- frontend/src/components/atoms/Icon.tsx
+++ frontend/src/components/atoms/Icon.tsx
@@ -8,34 +8,34 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({
-	styles,
-	imgUrl,
-	name,
-	isActive,
-	disabled,
-	handleClick,
+  styles,
+  imgUrl,
+  name,
+  isActive,
+  disabled,
+  handleClick,
 }: IconProps) => {
-	return (
-		<div
-			className={`rounded-[10px] ${
-				isActive && isActive === name && "bg-secondary"
-			} flex justify-center items-center ${!disabled && "cursor-pointer"} ${styles} ${
-				!styles && "w-[48px] h-[48px]"
-			} `}
-			onClick={handleClick}
-		>
-			{!isActive ? (
-				<img src={imgUrl} alt='fund_logo' className='w-1/2 h-1/2 cursor-pointer hover:grayscale' />
-			) : (
-				<img
-					src={imgUrl}
-					alt='fund_logo'
-					className={`w-1/2 h-1/2  ${
-						isActive !== name && "grayscale"
-					} cursor-pointer hover:grayscale-0 transition-all duration-300`}
-				/>
-			)}
-		</div>
-	)
+  return (
+    <div
+      className={`rounded-[10px] ${
+        isActive && isActive === name && "bg-secondary"
+      } flex justify-center items-center ${!disabled && "cursor-pointer"} ${styles} ${
+        !styles && "w-[48px] h-[48px]"
+      } `}
+      onClick={handleClick}
+    >
+      {!isActive ? (
+        <img src={imgUrl} alt='fund_logo' className='w-1/2 h-1/2 cursor-pointer hover:grayscale' />
+      ) : (
+        <img
+          src={imgUrl}
+          alt='fund_logo'
+          className={`w-1/2 h-1/2  ${
+            isActive !== name && "grayscale"
+          } cursor-pointer hover:grayscale-0 transition-all duration-300`}
+        />
+      )}
+    </div>
+  )
 }
 export default Icon
