1663f4e4cd95f325c99027a7848b6feca6ce3cd6
TerrniT <terrnit.megaman@gmail.com>
2023-03-06 11:27:17 +0500

    build(vite): production build produces rollup error "Could not resolve '../internals/globalThis'"

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter as Router } from "react-router-dom"
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react"
 import App from "./App"
-import "./style/global.css"
+import "./style/style.css"
 import { ThirdWebContextProvider } from "./context"
 
 const container = document.getElementById("root") as HTMLElement
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	define: {
-		global: "globalThis",
-		"process.env": {},
-	},
+  plugins: [react()],
+  define: {
+    global: "style",
+    "process.env": {},
+  },
 })
